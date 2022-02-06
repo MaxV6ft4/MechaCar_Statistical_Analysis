251303af4caa4333066e592efc400a43335817f9
Max Vincent <92235747+MaxV6ft4@users.noreply.github.com>
2022-02-06 13:21:15 -0500

    added t tests code to R script.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,3 +7,14 @@ summary(mpg_regress)
 Coil_table = read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors=F)
 total_summary = Coil_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
 lot_summary=Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups='keep')
+
+t.test(Coil_table$PSI, mu=1500)
+
+lot1=subset(Coil_table, Manufacturing_Lot == 'Lot1')
+t.test(lot1$PSI, mu=1500)
+
+lot2=subset(Coil_table, Manufacturing_Lot == 'Lot2')
+t.test(lot2$PSI, mu=1500)
+
+lot3=subset(Coil_table, Manufacturing_Lot == 'Lot3')
+t.test(lot3$PSI, mu=1500)
